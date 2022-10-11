957ceda6cfd12974a9745be4ee746337f04e43ee
Adam De Rango <113118048+adamdera@users.noreply.github.com>
2022-10-11 10:00:04 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print ("lets do some multiplication")
+
+A = int(input("Enter first positive integer:"))
+B = int(input("Enter second positive integer integer:"))
+
+while A <= B:
+  Number=A* B
+  print ("the number is", Number)
